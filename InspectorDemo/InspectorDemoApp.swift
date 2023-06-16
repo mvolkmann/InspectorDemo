0643cd4e8203a73a26b23afce90e3b210211a988
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-16 16:51:54 -0500

    Initial Commit

---
+++ InspectorDemo/InspectorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InspectorDemoApp.swift
+//  InspectorDemo
+//
+//  Created by Mark Volkmann on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct InspectorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
